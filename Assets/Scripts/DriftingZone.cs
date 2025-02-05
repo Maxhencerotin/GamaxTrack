85ab35b728d576ac0b0c50159fdb3b764aa3fa85
Maxhencerotin <max.henrotin@gmail.com>
2025-02-05 13:20:06 +0100

    minor change

--- Assets/Scripts/DriftingZone.cs
+++ Assets/Scripts/DriftingZone.cs
@@ -28,7 +28,7 @@ public class DriftingZone : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //maybe not the best solution for performances ?? I should maybe do like warning colour and handle 2 elements (despitebeing less modular)
         foreach (Transform gauge in driftingGauge.transform)
         {
             //update filling
@@ -40,39 +40,39 @@ public class DriftingZone : MonoBehaviour
             RectTransform cursorPosition = gauge.GetChild(0).GetComponent<RectTransform>();
             float NewCursorPositionX = gaugePosition.rect.width * car.GetSlidingLimit();
             cursorPosition.anchoredPosition = new Vector2(NewCursorPositionX, cursorPosition.anchoredPosition.y);
+        }
 
-            //check warning colour
-           
-            Color color = warningColourLeft.color;  //same colour for right
-            if (sliderControl.getSlidingValue() - car.GetSlidingLimit() <= DISTANCE_WARNING)  //when we are at DistanceWarning from the slidingLimit
+        //check warning colour
+
+        Color color = warningColourLeft.color;  //same colour for right
+        if (sliderControl.getSlidingValue() - car.GetSlidingLimit() <= DISTANCE_WARNING)  //when we are at DistanceWarning from the slidingLimit
+        {
+            /*
+            if (soundOn)
             {
-                /*
-                if (soundOn)
-                {
-                    Handheld.Vibrate();
-                }
-                */
-                if (sliderControl.getSlidingValue() - car.GetSlidingLimit() <= 0)
-                {
-                    color.a = 1.0f;
-                }
-                else
-                {
-                    //with rule of three : proportion of distances gives intensity of colour
-                    float alphaRange = WARNING_ALPHA_MAX - WARNING_ALPHA_MIN;
-                    float currentSegment = sliderControl.getSlidingValue() - car.GetSlidingLimit();
-                    color.a = alphaRange*((DISTANCE_WARNING -currentSegment)/DISTANCE_WARNING);  
-                }  
+                Handheld.Vibrate();
+            }
+            */
+            if (sliderControl.getSlidingValue() - car.GetSlidingLimit() <= 0)
+            {
+                color.a = 1.0f;
             }
             else
             {
-               
-                color.a = 0.0f; 
-                
+                //with rule of three : proportion of distances gives intensity of colour
+                float alphaRange = WARNING_ALPHA_MAX - WARNING_ALPHA_MIN;
+                float currentSegment = sliderControl.getSlidingValue() - car.GetSlidingLimit();
+                color.a = alphaRange * ((DISTANCE_WARNING - currentSegment) / DISTANCE_WARNING);
             }
-            warningColourLeft.color = color;
-            warningColourRight.color = color;
         }
+        else
+        {
+
+            color.a = 0.0f;
+
+        }
+        warningColourLeft.color = color;
+        warningColourRight.color = color;
 
     }
 }
